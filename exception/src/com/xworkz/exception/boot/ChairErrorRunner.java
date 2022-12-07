d3d32b86092f44de263b1bc283184cf9ea03af39
SAHANAGN <sahanagnxworkz@gmail.com>
2022-12-07 07:20:45 +0530

    Excepton-->error

---
+++ exception/src/com/xworkz/exception/boot/ChairErrorRunner.java
@@ -0,0 +1,40 @@
+package com.xworkz.exception.boot;
+
+import com.xworkz.exception.type.Chair;
+
+public class ChairErrorRunner {
+	public static void main(String[] args) {
+
+		Chair chair = new Chair();
+
+		chair.manjula();
+		chair.bag();
+		chair.bags();
+		chair.blue();
+		chair.sky();
+		chair.pink();
+		chair.black();
+		chair.red();
+		chair.laptop();
+		chair.pen();
+		chair.bluePen();
+		chair.chair();
+		chair.sahana();
+		chair.swati();
+		chair.harisha();
+		chair.harshi1();
+		chair.neelu();
+		chair.jeevika();
+		chair.jothi();
+		chair.preethu();
+		chair.nayana();
+		chair.sneha();
+		chair.kirthana();
+		chair.shilpa();
+		chair.aishwarya();
+		chair.sunitha();
+		chair.divya();
+		chair.teju();
+		chair.bhuvana();
+	}
+}
---
+++ exception/src/com/xworkz/exception/type/Chair.java
@@ -0,0 +1,158 @@
+package com.xworkz.exception.type;
+
+import java.awt.AWTError;
+import java.io.IOError;
+import java.nio.charset.CoderMalfunctionError;
+import java.util.ServiceConfigurationError;
+import javax.xml.parsers.FactoryConfigurationError;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.validation.SchemaFactoryConfigurationError;
+
+public class Chair {
+
+	public void manjula() throws AssertionError {
+		System.out.println("running method");
+		throw new AssertionError(null);
+	}
+
+	public void bag() throws AWTError {
+		System.out.println("running method");
+		throw new AWTError(null);
+	}
+
+	public void bags() throws CoderMalfunctionError {
+		System.out.println("running method");
+		throw new CoderMalfunctionError(null);
+	}
+
+	public void blue() throws FactoryConfigurationError {
+		System.out.println("running method");
+		throw new FactoryConfigurationError();
+	}
+
+	public void sky() throws FactoryConfigurationError {
+		System.out.println("running method");
+		throw new FactoryConfigurationError();
+	}
+
+	public void pink() throws IOError {
+		System.out.println("running method");
+		throw new IOError(null);
+	}
+
+	public void black() throws LinkageError {
+		System.out.println("running method");
+		throw new LinkageError();
+	}
+
+	public void red() throws LinkageError {
+		System.out.println("running method");
+		throw new LinkageError();
+	}
+
+	public void laptop() throws LinkageError {
+		System.out.println("running method");
+		throw new LinkageError();
+	}
+
+	public void pen() throws SchemaFactoryConfigurationError {
+		System.out.println("running method");
+		throw new SchemaFactoryConfigurationError();
+	}
+
+	public void bluePen() throws SchemaFactoryConfigurationError {
+		System.out.println("running method");
+		throw new SchemaFactoryConfigurationError();
+	}
+
+	public void chair() throws SchemaFactoryConfigurationError {
+		System.out.println("rug method");
+		throw new SchemaFactoryConfigurationError();
+	}
+
+	public void sahana() throws SchemaFactoryConfigurationError {
+		System.out.println("running method");
+		throw new SchemaFactoryConfigurationError();
+	}
+
+	public void swati() throws SchemaFactoryConfigurationError {
+		System.out.println("running method");
+		throw new SchemaFactoryConfigurationError(null, null);
+	}
+
+	public void harisha() throws ServiceConfigurationError {
+		System.out.println("running method");
+		throw new ServiceConfigurationError(null);
+	}
+
+	public void harshi1() throws ServiceConfigurationError {
+		System.out.println("running method");
+		throw new ServiceConfigurationError(null, null);
+	}
+
+	public void neelu() throws ThreadDeath {
+		System.out.println("running method");
+		throw new ThreadDeath();
+	}
+
+	public void jeevika() throws TransformerFactoryConfigurationError {
+		System.out.println("running method");
+		throw new TransformerFactoryConfigurationError();
+	}
+
+	public void jothi() throws TransformerFactoryConfigurationError {
+		System.out.println("running method");
+		throw new TransformerFactoryConfigurationError();
+	}
+
+	public void preethu() throws TransformerFactoryConfigurationError {
+		System.out.println("running method");
+		throw new TransformerFactoryConfigurationError();
+	}
+
+	public void nayana() throws AssertionError {
+		System.out.println("running method");
+		throw new AssertionError();
+	}
+
+	public void sneha() throws AssertionError {
+		System.out.println("running method");
+		throw new AssertionError();
+	}
+
+	public void kirthana() throws AssertionError {
+		System.out.println("running method");
+		throw new AssertionError();
+	}
+
+	public void shilpa() throws AssertionError {
+		System.out.println("running method");
+		throw new AssertionError();
+	}
+
+	public void aishwarya() throws AssertionError {
+		System.out.println("running method");
+		throw new AssertionError();
+	}
+
+	public void sunitha() throws AssertionError {
+		System.out.println("running method");
+		throw new AssertionError(0);
+	}
+
+	public void divya() throws AssertionError {
+		System.out.println("running method");
+		throw new AssertionError(0);
+	}
+
+	public void teju() throws AssertionError {
+		System.out.println("running method");
+		throw new AssertionError(getClass());
+	}
+
+	public void bhuvana() throws javax.xml.stream.FactoryConfigurationError {
+		System.out.println("running method");
+		throw new FactoryConfigurationError(null, null);
+	}
+
+}
